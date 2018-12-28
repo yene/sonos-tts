21f52a14d3173de6238d8bf3e7398182577152cf
Yannick Weiss <contact@yannickweiss.com>
2018-12-28 19:50:30 +0100

    Using player data form the API and sending doorbell or speech command back to the server.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import m from 'mithril';
 
-var SonosAuth = require('./views/SonosAuth.js');
 var SonosTTS = require('./views/SonosTTS.js')
 
 var app = document.querySelector('.app');
-m.route(app, '/auth', {
-  '/auth': SonosAuth,
-  '/tts': SonosTTS,
+m.route(app, '/', {
+  '/': SonosTTS,
 })
--- src/views/SonosTTS.js
+++ src/views/SonosTTS.js
@@ -1,42 +1,80 @@
 
 var m = require('mithril');
 
-var data = [{
-    name: 'home 1',
-    players: [
-      {name: 'player 1', id: '123'},
-      {name: 'player 2', id: '1234'},
-      {name: 'player 3', id: '12345'},
-    ]
-  }, {
-    name: 'home 2',
-    players: [
-      {name: 'player xy', id: '123'},
-    ]
-  }];
-
-module.exports = {
+var SonosTTS = {
+  data: [{
+    name: 'loading...',
+    players: [{
+      id: '',
+      name: 'loading...',
+    }],
+  }],
+  selectedPlayer: '',
+  textToSpeak: '',
   oninit: function(vnode) {
-    // TODO: load homes and the players of the homes
+    this.loadPlayers();
   },
-  view: function() {
+  loadPlayers: function() {
+    return m.request({
+      method: 'GET',
+      url: window.location.protocol + '//' + window.location.host + '/players',
+      withCredentials: true,
+    })
+    .then(function(result) {
+      if (result.success === false || result.households === undefined) {
+        if (result.authRequired !== undefined && result.authRequired === true) {
+          window.location.href = '/auth';
+          return;
+        }
+        console.log('Unexpected error happend', result);
+        return;
+      }
+      SonosTTS.data = result.households;
+    })
+  },
+  view: function(vnode) {
     return m('.app-tts', [
       m('h2', 'Sonos Text to Speech'),
       m('span', 'Select your target player '),
       m('select',
-        data.map(function(home) {
+        {onchange: function(e) {
+          SonosTTS.selectedPlayer = e.target.options[e.target.selectedIndex].value;
+        }},
+        m('option', {value: '', selected: 'true'}, 'Choose a player'),
+        this.data.map(function(home) {
           return m('optgroup[label=' + home.name + ']',
             home.players.map(function(player) {
-              return m('option[value=' + player.id + ']', player.name);
+              return m('option',
+                {value: player.id, selected: SonosTTS.selectedPlayer === player.id},
+                player.name
+              );
             })
           );
         })
       ),
       m('', [
         m('span', 'Enter your text to speak: '),
-        m('input.input[placeholder=Good Morning]'),
+        m('input.input[placeholder=Good Morning]', {
+          oninput: function (e) {SonosTTS.textToSpeak = e.target.value},
+          value: SonosTTS.textToSpeak
+        }),
       ]),
-      m('button.button[type=button]', 'Send')
+      m('button.button[type=button]', {disabled: SonosTTS.selectedPlayer === '' || SonosTTS.textToSpeak === '' , onclick: function() {
+        m.request({
+          method: 'GET',
+          url: window.location.protocol + '//' + window.location.host + '/speakText?playerId=' + SonosTTS.selectedPlayer + '&text=' + SonosTTS.textToSpeak,
+          withCredentials: true,
+        })
+      }}, 'Play Text'),
+      m('button.button[type=button]', {disabled: SonosTTS.selectedPlayer === '', onclick: function() {
+        m.request({
+          method: 'GET',
+          url: window.location.protocol + '//' + window.location.host + '/doorbell?playerId=' + SonosTTS.selectedPlayer,
+          withCredentials: true,
+        })
+      }}, 'Play Doorbell')
     ]);
   }
 }
+
+module.exports = SonosTTS;
